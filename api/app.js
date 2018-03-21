3be62911b9a4c4cb513681c44b8bde344185b58a
Celine Kim <celinekim.kim@mail.utoronto.ca>
2018-03-21 19:15:05 -0400

    user endpoint definition draft 1

--- api/app.js
+++ api/app.js
@@ -49,7 +49,18 @@ app.get('/', (req, res) => {
 });
 
 // RESTful Routes
-app.get('/user', user.functionName);
+
+// CRUD for session
+app.post('/user/session', user.session_post);
+app.get('/user/session', user.session_get);
+app.del('/user/session', user.session_del);
+app.put('/user/session', user.session_put);
+
+//CRUD for user management
+app.put('/user/user', user.user_put); // Create new user
+app.get('/user/user/:id', user.user_get); // Get user info with username :id
+app.post('/user/user/:id', user.user_post); // Update user info with username :id
+app.del('/user/user/:id', user.user_del); // Delete user with username :id
 
 
 // When running tests, we don't want to start up the server on its own.
