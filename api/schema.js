4e42caebb41f279ff235b0bb3b089ba86ea644f3
Dominick Han <dominick.han@mail.utoronto.ca>
2018-03-21 22:23:18 -0400

    Added mongo connection

--- api/schema.js
+++ api/schema.js
@@ -1,14 +1,15 @@
 let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+
+
 
 let cryptoSchema = new Schema({
 	name: {type: String, required: true, unique: true },
 	symbol: {type: String, required: true, unique: true }
 });
-
 export const Crypto = mongoose.model('Crypto', cryptoSchema);
 
-let userSchema = new Schema({
+
+let userSchema = new mongoose.Schema({
 	username: {type: String, required: true, unique: true },
 	password: {type: String, required: true},
 	firstName: {type: String},
@@ -22,21 +23,30 @@ let userSchema = new Schema({
 	}],
 	balance: {type: Number, required: true, default: 5000}
 });
-
 export const User = mongoose.model('User', userSchema);
 
-let transactionSchema = new Schema({
+
+let transactionSchema = new mongoose.Schema({
 	currencyID: {type: Schema.Types.ObjectId, ref: 'Crypto'},
 	userID: {type: Schema.Types.ObjectId, ref: 'User'},
 	transactionAt: {type: Date},
 	amount: {type: Number}
 });
-
 export const Transaction = mongoose.model('Transaction', transactionSchema);
 
-let sessionSchema = new Schema({
+
+let sessionSchema = new mongoose.Schema({
 	userID: {type: Schema.Types.ObjectId, ref: 'User'},
 	sessionToken: {type: String}
 });
-
 export const Session = mongoose.model('Session', sessionSchema);
+
+
+
+mongoose.connect('mongodb://localhost/monus', (error) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Database connection successful');
+	}
+});
