d86265746924bb71a30a974843b14d6822d39402
Dominick Han <dominick.han@mail.utoronto.ca>
2018-03-22 00:49:02 -0400

    Updated app

--- api/app.js
+++ api/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const schema = require('./schema');
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Support Routes
-app.get('/', (req, res) => {
-  console.log(req);
-  res.send('hello world');
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 // RESTful Routes
 
 // User management
 // Create new user
-app.post('/user', () => {
-
+app.post('/new_user', (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
 });
 // Login
 app.post('/login', () => {
@@ -28,8 +32,9 @@ app.post('/logout', () => {
 
 // Currency/Transaction
 // Get current wallet/balance
-app.get('/wallet', () => {
-
+app.get('/wallet', (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
 });
 // Create a new transaction
 app.post('/transaction', () => {
